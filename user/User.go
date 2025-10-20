c49fc15889b0009c1b31ee3c3443749fdf38ab25
lowe <kainkent69@gmail.com>
2025-10-20 05:00:48 -0700

    chore: update

--- user/User.go
+++ user/User.go
@@ -1,6 +1,74 @@
 package user
 
+import (
+	"encoding/json"
+	"errors"
+)
+
+// the user informations.
 type User struct {
-	Fname
+	// accont information
+	Fname string `json:"name.first"`
+	Lname string `json:"name.last"`
+	// username and email.
+	UserName string `json:"username"`
+	Email    string `json:"email"`
+	// password
+	Password string `json:"password"`
+
+	//  coins and shards.
+	Shards string `json:"resource.shards"`
+	Coins  string `json:"resource.coins"`
+
+	// tax and others
+	Level uint `json:"player.level"`
+	Tax   uint `json:"player.tax"`
 }
 
+// For LogIn
+
+type RegisterAuth struct {
+	EmailUsername string `json:"auth.emailOrUsername"`
+	Password      string `json:"auth.password"`
+}
+
+// the information for registering
+type RegisterInfo struct {
+	RegisterAuth
+	Fname string `json:"name.first"`
+	Lname string `json:"name.last"`
+}
+
+// convert string to User
+func (u User) FromJson(user string) (*User, error) {
+	newUser := new(User)
+	err := json.Unmarshal([]byte(user), newUser)
+	if err != nil {
+		return newUser, err
+	}
+	return newUser, nil
+}
+
+// convert a user back json
+
+func (u *User) ToJson() ([]byte, error) {
+	data, err := json.Marshal(u)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+
+}
+
+// Validate register
+
+func Validate(u *RegisterInfo) (bool, []byte, error) {
+	found := new(User)
+	if ok := len(found.Lname) >= 3 || len(found.Fname) >= 3; !ok {
+		return false, []byte(`user.Validate: Invalid Name`), errors.New("error")
+	}
+
+	return true, []byte("user.Validate: Success"), nil
+
+}
